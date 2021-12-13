b81e9661b2749cb61419105731624510e45e5d02
AKim47 <abel.kim.22@gmail.com>
2021-12-13 01:10:32 -0600

    added POST API/notes

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
+
+const fs = require('fs');
 
 const path = require('path');
 
 const notes = require('./Develop/db/db.json');
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+
+// parse incoming JSON data
+app.use(express.json());
 
 // return index.html
 //remember to remove Develop
@@ -29,10 +39,31 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+function newNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './Develop/db/db.json'),
+        JSON.stringify({ notes: notesArray}, null, 2)
+    );
+    return body;
+}
+
 // save new note to db.json, and return new note to client
 app.post('/api/notes', (req, res) => {
+    //set id based on what the next index of array is
+    req.body.id = notes["notes"].length.toString();
     
+    const note = newNote(req.body, notes["notes"]);
+
+    res.json(note);
 });
+
+// function findById(id, notesArray) {
+//     const result = notesArray.filter(notes => notes.id === id)[0];
+//     return result;
+//   };
 
 // DELETE /api/notes/:id
 //app.delete('/api/notes/:id', (req, res) =>)
+//const result = findById(req.params.id, notes);
