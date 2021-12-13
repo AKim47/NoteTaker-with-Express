31b97011bb8e5403a1ad5639d9e80ea00f5a168d
AKim47 <abel.kim.22@gmail.com>
2021-12-12 23:40:04 -0600

    added basic html routes

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+//const { notes } = require('./db/db.json');
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
+
+// return index.html
+//remember to remove Develop
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
+});
+
+// return notes.html
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));
+});
+
+// read db.json and return all saved notes as JSON
+
+app.get('/api/notes', (req, res) => {
+
+});
+
+// save new note to db.json, and return new note to client
+app.post('/api/notes', (req, res) => {
+
+});
+
+// DELETE /api/notes/:id
+//app.delete('/api/notes/:id', (req, res) =>)
